3233c0c95f987769eef518ae689be586549a157d
Vyacheslav Chernyak <Vachernyak@mts.ru>
2023-12-13 21:33:17 +0300

    Fixid encoding

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def get_info_vacancy():
         for u in url_all:
             url = u.get_attribute('href')
             info = BeautifulSoup(get_info_url(url).text, "lxml")
-            descr = info.find('div', class_='g-user-content').text
+            # descr = info.find('div', class_='g-user-content').text
+            descr = info.find('div', {'data-qa': 'vacancy-description'}).text
             if re.search(r'Django', descr) or re.search(r'Flask', descr):
                 salary_element = info.find('div', {'data-qa': 'vacancy-salary'})
                 salary = salary_element.text.strip() if salary_element else "Зарплата не указана"
@@ -37,7 +38,6 @@ def get_info_vacancy():
                     address_element = info.find('span', {'data-qa': 'vacancy-view-raw-address'}).text
                 else:
                     address_element = info.find('p', {'data-qa': 'vacancy-view-location'}).text
-                # address = address_element.text.strip() if address_element else "Адрес не указан"
                 company = info.find('span', {'data-qa': 'bloko-header-2'}).text
                 title = info.find('h1', class_='bloko-header-section-1').text
                 # print(title, '\n', url, '\n', company, '\n', address_element, '\n', salary)
@@ -54,5 +54,6 @@ def get_info_vacancy():
 if __name__ == '__main__':
     info_vacancy = get_info_vacancy()
     with open('files/log.json', 'a', encoding='utf-8-sig') as file:
-        json.dump(info_vacancy, file)
+        json.dump(info_vacancy, file, ensure_ascii=False)
 
+    print("END")
